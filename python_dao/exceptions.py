4f05051a1b4c83494e276ebf4c774b36d29ce58c
Maxime Boulanger <51764404+mbml84@users.noreply.github.com>
2023-01-27 13:59:09 -0500

    Feature/pickle serializaton (#3)
    * feature:Use pickle instead of json
    
    * feature:Use class based decorator factory

--- python_dao/exceptions.py
+++ python_dao/exceptions.py
@@ -3,17 +3,25 @@ Exceptions for data fetching
 """
 from __future__ import annotations
 
+from typing import Callable
+
 
 class NoResultFound(BaseException):
     """
     Exception for cases when no result are returned but at least one was expected
     """
 
+    def __init__(self, func: Callable):
+        super().__init__(f'No result found found for {func.__name__}')
+
 
 class MultipleResultFound(BaseException):
     """
     Exception for cases when multiple results were returned but only one was expected
     """
+
+    def __init__(self, func: Callable):
+        super().__init__(f'Multiple results returned for {func.__name__}')
 
 
 __all__ = [
